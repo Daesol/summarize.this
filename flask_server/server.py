75c3b5a27cfa6c6f6294b5357816021bc49c6875
daesol <daesol97@gmail.com>
2023-07-13 12:14:49 -0400

    minor fix

--- flask_server/server.py
+++ flask_server/server.py
@@ -1,20 +1,22 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 from youtube_transcript_api import YouTubeTranscriptApi
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
+CORS(app)  # Enable CORS
+
+def extract_video_id(video_link):
+    return video_link.split('=')[-1]
 
 @app.route('/transcript', methods=['GET', 'POST'])
-@cross_origin()  # Allow CORS for this specific route
 def get_transcript():
     if request.method == 'POST':
         video_link = request.json['videoLink']
         video_id = extract_video_id(video_link)
-
+        
         print(f"Received request for video link: {video_link}")
         print(f"Extracted video ID: {video_id}")
-
+        
         # Fetch transcript using youtube-transcript-api
         try:
             transcript = YouTubeTranscriptApi.get_transcript(video_id)  # Use video ID, not link
@@ -27,3 +29,4 @@ def get_transcript():
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
+
